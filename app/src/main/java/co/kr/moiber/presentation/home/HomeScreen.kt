818a600b4b9408ec9a02fad52fc6acefb47761c4
eunwoo <eunwoo@finda.co.kr>
2024-08-21 18:59:41 +0900

    feat: 애니메이션 추가

--- app/src/main/java/co/kr/moiber/presentation/home/HomeScreen.kt
+++ app/src/main/java/co/kr/moiber/presentation/home/HomeScreen.kt
@@ -1,5 +1,8 @@
 package co.kr.moiber.presentation.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -11,7 +14,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -20,6 +28,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import co.kr.moiber.model.FakeHomeWeatherSummary
 import co.kr.moiber.model.WeatherMessage
+import co.kr.moiber.presentation.home.components.animation.HomeAnimationVisibility
 import co.kr.moiber.presentation.home.components.card.WeatherCard
 import co.kr.moiber.presentation.home.components.header.TopHeaderView
 import co.kr.moiber.presentation.home.components.message.WeatherMessageView
@@ -30,6 +39,7 @@ import co.kr.moiber.shared.ui.black
 import co.kr.moiber.shared.ui.black02
 import co.kr.moiber.shared.ui.white01
 import co.kr.moiber.shared.ui.yellow02
+import kotlinx.coroutines.delay
 import java.util.Date
 
 @Composable
@@ -47,46 +57,95 @@ private fun HomeScreen(
     state: HomeState,
     onEvent: (HomeViewEvent) -> Unit
 ) {
+    var isVisible by remember { mutableStateOf(false) }
     val weatherSummary = state.weatherSummary
     val bgColor = if (weatherSummary?.isDay == true) yellow02 else black02
+
+    LaunchedEffect(Unit) {
+        delay(150)
+        isVisible = true
+    }
     weatherSummary?.let {
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(bgColor)
         ) {
-            Image(
+            Column(
                 modifier = Modifier.align(Alignment.CenterStart),
-                painter = painterResource(id = weatherSummary.weatherBgResId),
-                contentDescription = null
-            )
-            Image(
+            ) {
+                HomeAnimationVisibility(
+                    visible = isVisible,
+                    duration = 400,
+                    delay = 150
+                ) {
+                    Image(
+                        painter = painterResource(id = weatherSummary.weatherBgResId),
+                        contentDescription = null
+                    )
+                }
+            }
+            Column(
                 modifier = Modifier.align(Alignment.BottomEnd),
-                painter = painterResource(id = weatherSummary.characterResId),
-                contentDescription = null
-            )
+            ) {
+                HomeAnimationVisibility(
+                    isSlide = true,
+                    visible = isVisible,
+                    duration = 300,
+                    delay = 350 + (200 * weatherSummary.weatherMessageList.size) + 500
+                ) {
+                    Image(
+                        painter = painterResource(id = weatherSummary.characterResId),
+                        contentDescription = null
+                    )
+                }
+            }
             Column {
-                TopHeaderView(
-                    isDay = weatherSummary.isDay
-                )
-                WeatherCard(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    weatherSummary = state.weatherSummary
-                )
+                HomeAnimationVisibility(
+                    visible = isVisible,
+                    duration = 400,
+                    delay = 150
+                ) {
+                    TopHeaderView(
+                        isDay = weatherSummary.isDay
+                    )
+                }
+                HomeAnimationVisibility(
+                    visible = isVisible,
+                    duration = 1200,
+                    delay = 250
+                ) {
+                    WeatherCard(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        weatherSummary = state.weatherSummary
+                    )
+                }
                 Spacer(modifier = Modifier.size(16.dp))
                 Column(
                     modifier = Modifier.padding(horizontal = 22.dp)
                 ) {
-                    DayNightText(
-                        style = Body04,
-                        text = Date().toFormatString("M월 d일(E)") ?: "-",
-                        isDay = weatherSummary.isDay
-                    )
-                    Spacer(modifier = Modifier.size(6.dp))
-                    weatherSummary.weatherMessageList.forEach { weatherMessage ->
-                        WeatherMessageView(
-                            weatherMessage = weatherMessage
+                    HomeAnimationVisibility(
+                        visible = isVisible,
+                        duration = 250,
+                        delay = 350
+                    ) {
+                        DayNightText(
+                            style = Body04,
+                            text = Date().toFormatString("M월 d일(E)") ?: "-",
+                            isDay = weatherSummary.isDay
                         )
+                    }
+                    Spacer(modifier = Modifier.size(6.dp))
+                    weatherSummary.weatherMessageList.forEachIndexed { index, weatherMessage ->
+                        HomeAnimationVisibility(
+                            visible = isVisible,
+                            duration = 200,
+                            delay = 350 + (200 * index)
+                        ) {
+                            WeatherMessageView(
+                                weatherMessage = weatherMessage
+                            )
+                        }
                         Spacer(modifier = Modifier.size(14.dp))
                     }
                 }
---
+++ app/src/main/java/co/kr/moiber/presentation/home/components/animation/HomeAnimationVisibility.kt
@@ -0,0 +1,40 @@
+package co.kr.moiber.presentation.home.components.animation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeAnimationVisibility(
+    isSlide: Boolean = false,
+    visible: Boolean,
+    duration: Int,
+    delay: Int,
+    content: @Composable () -> Unit
+) {
+    var animation: EnterTransition = fadeIn(
+        animationSpec = tween(
+            durationMillis = duration, // 1초 동안 애니메이션
+            delayMillis = delay        // 1초 지연 후 애니메이션 시작
+        )
+    )
+    AnimatedVisibility(
+        visible = visible,
+        enter = if (isSlide) {
+            animation + slideInVertically(
+                initialOffsetY = { -40 }, // -40dp 위에서 시작
+                animationSpec = tween(
+                    durationMillis = duration, // 1초 동안 애니메이션
+                    delayMillis = delay        // 1초 지연 후 애니메이션 시작
+                )
+            )
+        } else {
+            animation
+        }
+    ) {
+        content()
+    }
+}
