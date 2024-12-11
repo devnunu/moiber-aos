6e2af7d8b521a43e8377cc78f0a9f98836a570cf
eunwoo <aufcl4858@naver.com>
2024-12-11 15:54:26 +0900

    feat: Date View 추가

---
+++ app/src/main/java/co/kr/moiber/presentation/feature/weather/detail/components/dateview/WeatherDateView.kt
@@ -0,0 +1,11 @@
+package co.kr.moiber.presentation.feature.weather.detail.components.dateview
+
+import androidx.compose.runtime.Composable
+import co.kr.moiber.model.weather.WeatherByDate
+
+@Composable
+fun WeatherDateView(
+    weatherByDateList: List<WeatherByDate>
+) {
+
+}
---
+++ app/src/main/java/co/kr/moiber/presentation/feature/weather/detail/components/dateview/WeatherDateViewItem.kt
@@ -0,0 +1,11 @@
+package co.kr.moiber.presentation.feature.weather.detail.components.dateview
+
+import androidx.compose.runtime.Composable
+import co.kr.moiber.model.weather.WeatherByDate
+
+@Composable
+fun WeatherDateViewItem(
+    weatherByDate: WeatherByDate
+) {
+
+}
